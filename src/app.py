9f2a7d7b852ec428feb9645216ba3024a18f2260
Emifacio <127351922+Emifacio@users.noreply.github.com>
2023-11-29 21:14:49 -0300

    conexión con la base de datos

--- src/app.py
+++ src/app.py
@@ -5,5 +5,24 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 
+app.config ['MYSQL_DATABASE_HOST'] = 'localhost:3306'
+app.config ['MYSQL_DATABASE_USER'] = 'root'
+app.config ['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config ['MYSQL_DATABASE_DB'] = 'cursos'
+
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql = "insert into cursos (nombre) value ('trading');"
+    cursor.execute(sql)
+
+    conn.commit(sql)
+
+    return render_template('cursos/index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
