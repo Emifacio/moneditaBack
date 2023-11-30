055f7549cd6ec018a28d9ae5f9943746b62b2fd8
Emifacio <127351922+Emifacio@users.noreply.github.com>
2023-11-29 21:28:34 -0300

    update

--- src/app.py
+++ src/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask import render_template
 from flaskext.mysql import MySQL
 
+
 app = Flask(__name__)
 mysql = MySQL()
 
-app.config ['MYSQL_DATABASE_HOST'] = 'localhost:3306'
+app.config ['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config ['MYSQL_DATABASE_USER'] = 'root'
 app.config ['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config ['MYSQL_DATABASE_DB'] = 'cursos'
